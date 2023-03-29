94aadd03b3fb22d888f53a0eb6b4a62b8e541d32
KamilRaszkiewicz <Kamil.Raszkiewicz02@gmail.com>
2023-03-29 11:23:58 +0200

    Implemented form handling

--- PB03/Pages/Index.cshtml.cs
+++ PB03/Pages/Index.cshtml.cs
@@ -8,11 +8,27 @@ namespace PB03.Pages
     {
         [BindProperty]
         public DateForm DateForm { get; set; }
+
+        public string Message => GetMessage(DateForm.Name, DateForm.Year);
         private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
+        }
+
+        private string GetMessage(string name, int year)
+        {
+            bool isGirl = false;
+
+            if (name.Last() == 'a')
+            {
+                isGirl = true;
+            }
+
+            bool isLeap = DateTime.IsLeapYear(year);
+
+            return $"{name} urodził{(isGirl ? "a" : string.Empty)} się w {DateForm.Year} roku. To {(isLeap ? "nie " : string.Empty)}był rok przestępny.";
         }
 
         public IActionResult OnPost()
